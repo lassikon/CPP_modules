b621b034c2f9d34c10284a89c9b053053864026e
Lassi Konttinen <lkonttin@c1r6p18.hive.fi>
2024-07-04 12:33:33 +0300

    copied Zombie class from ex00

--- cpp01/ex01/Zombie.cpp
+++ cpp01/ex01/Zombie.cpp
@@ -10,3 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << "Zombie " << this->name << " has been destroyed." << std::endl;
+}
+
+void Zombie::announce(void)
+{
+	std::cout << this->name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
