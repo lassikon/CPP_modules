d67171c1a7a5eb404ee6041cf376e562cb56287c
Lassi Konttinen <lassikonttinen@gmail.com>
2024-08-06 16:09:13 +0300

    ex03 initial commit

---
+++ cpp04/ex03/AMateria.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkonttin <lkonttin@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 16:00:01 by lkonttin          #+#    #+#             */
+/*   Updated: 2024/08/06 16:08:41 by lkonttin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+# include "ICharacter.hpp"
+# include <string>
+
+class ICharacter;
+
+class AMateria
+{
+	protected:
+		std::string	type;
+
+	public:
+		AMateria();
+		AMateria(std::string const & type);
+		AMateria(const AMateria& other);
+		virtual ~AMateria();
+
+		AMateria&	operator=(const AMateria& other);
+
+		std::string const & getType() const; //Returns the materia type
+
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
+
+#endif
---
+++ cpp04/ex03/ICharacter.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkonttin <lkonttin@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 16:06:46 by lkonttin          #+#    #+#             */
+/*   Updated: 2024/08/06 16:07:48 by lkonttin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+
+# include "AMateria.hpp"
+# include <string>
+
+class ICharacter
+{
+	public:
+		virtual ~ICharacter() {}
+		virtual std::string const & getName() const = 0;
+		virtual void equip(AMateria* m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter& target) = 0;
+};
+
+#endif
