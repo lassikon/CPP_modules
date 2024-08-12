45caaa66910ebb5448b81ff7bd51f95aa1026bb4
Lassi Konttinen <lkonttin@c2r6p2.hive.fi>
2024-08-12 10:55:40 +0300

    more tests

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lkonttin <lkonttin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 11:51:14 by lkonttin          #+#    #+#             */
-/*   Updated: 2024/08/07 20:03:48 by lkonttin         ###   ########.fr       */
+/*   Updated: 2024/08/12 10:52:42 by lkonttin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,34 @@
 #include "WrongCat.hpp"
 #include <iostream>
 
-int main()
+void    testFromSubject()
 {
-    int n = 100;
-    Animal *animals[100];
-    for (int i = 0; i < n; i++)
+    std::cout << "---------------------" << std::endl;
+    std::cout << "TEST FROM THE SUBJECT" << std::endl;
+    std::cout << "---------------------" << std::endl;
+
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+    delete j;//should not create a leak
+    delete i;
+}
+
+void    animalArrayTest()
+{
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "ARRAY OF 10 ANIMALS, 5 CATS & 5 DOGS" << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+
+    Animal *animals[10];
+    for (int i = 0; i < 10; i++)
     {
         if (i < 50)
             animals[i] = new Dog();
         else
             animals[i] = new Cat();
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 10; i++)
     {
         std::cout << "memory address of " << (animals[i]->getType()) << ": " << &animals[i] << " and its brain: ";
         if (animals[i]->getType() == "Cat")
@@ -37,8 +53,58 @@ int main()
         else
             std::cout << dynamic_cast<Dog*>(animals[i])->getBrain() << std::endl;
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 10; i++)
     {
         delete animals[i];
     }
+}
+
+void    deepCopyTest()
+{
+    std::cout << "---------------" << std::endl;
+    std::cout << "DEEP COPY TESTS" << std::endl;
+    std::cout << "---------------" << std::endl;
+
+    Dog *dog = new Dog();
+    (*dog).getBrain()->setIdea("Original idea 1", 0);
+    (*dog).getBrain()->setIdea("Original idea 2", 50);
+
+    std::cout << "ORIGINAL IDEAS:" << std::endl;
+    std::cout << (*dog).getBrain()->getIdea(0) << std::endl;
+    std::cout << (*dog).getBrain()->getIdea(50) << std::endl;
+    std::cout << std::endl;
+
+    Dog *dogCopy = new Dog(*dog);
+    std::cout << "DOG COPY'S IDEAS:" << std::endl;
+    std::cout << (*dogCopy).getBrain()->getIdea(0) << std::endl;
+    std::cout << (*dogCopy).getBrain()->getIdea(50) << std::endl;
+    std::cout << std::endl;
+    
+    (*dogCopy).getBrain()->setIdea("New idea 1", 0);
+    (*dogCopy).getBrain()->setIdea("New idea 2", 50);
+    std::cout << "DOG COPY'S NEW IDEAS:" << std::endl;
+    std::cout << (*dogCopy).getBrain()->getIdea(0) << std::endl;
+    std::cout << (*dogCopy).getBrain()->getIdea(50) << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "ORIGINAL DOG'S IDEAS:" << std::endl;
+    std::cout << (*dog).getBrain()->getIdea(0) << std::endl;
+    std::cout << (*dog).getBrain()->getIdea(50) << std::endl;
+    std::cout << std::endl;
+
+    delete dog;
+    delete dogCopy;
+}
+
+void    assignmentOperatorTest()
+{
+    
+}
+
+int main()
+{
+    testFromSubject();
+    animalArrayTest();
+    deepCopyTest();
+    assignmentOperatorTest();
 }
