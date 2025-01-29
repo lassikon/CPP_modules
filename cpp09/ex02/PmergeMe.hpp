2fb3e08eae89b42bdf82f79d6bbd884d2a6930df
Lassi Konttinen <lassikonttinen@gmail.com>
2025-01-29 22:03:28 +0200

    cpp09/ex02 changed PmergeMe to a class template

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: lkonttin <lkonttin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 11:55:22 by lkonttin          #+#    #+#             */
-/*   Updated: 2025/01/29 21:19:24 by lkonttin         ###   ########.fr       */
+/*   Updated: 2025/01/29 21:53:54 by lkonttin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,51 +19,206 @@
 #include <ostream> // for printSequence()
 #include <vector>
 
-class PmergeMe {
+template <typename Container> class PmergeMe {
 public:
+  PmergeMe();
+  ~PmergeMe();
+
+  Container sort(const Container &input);
+
+private:
   struct Pair {
-    std::vector<int> small;
-    std::vector<int> large;
+    Container small;
+    Container large;
     size_t indexInMainSequence;
   };
 
-public:
-  PmergeMe();
-  PmergeMe(const PmergeMe &other) = delete;
-  PmergeMe &operator=(const PmergeMe &other) = delete;
-  ~PmergeMe();
-
-  void fordJohnsonAlgorithm();
-  void binaryInsertionSort();
+  void createJacobsthalSequence();
   void formMainSequence();
+  void updatePairIndexes(int inserted);
+  int getNextJacobsthal();
+  void binaryInsertionSort();
   void makePairs();
   void sortPairs();
-  void updatePairIndexes(int inserted);
-  void createJacobsthalSequence();
-  int getNextJacobsthal();
-  std::vector<int> sortVector(const std::vector<int> &input);
+  void fordJohnsonAlgorithm();
 
-  std::deque<int> sortDeque(const std::deque<int> &input);
+  Container seq;
+  std::vector<Pair> pairs;
+  Container oddElement;
+  Container leftovers;
+  std::vector<size_t> jacobsthalSequence;
+  size_t elementSize;
+  size_t jacobsthalIndex;
+  bool keepRecursing;
+};
 
-  template <typename Container> void printSequence(const Container &sequence) {
-    for (size_t i = 0; i < sequence.size(); i++) {
-      std::cout << sequence[i] << (i < sequence.size() - 1 ? " " : "\n");
+template <typename Container> PmergeMe<Container>::PmergeMe() {
+  createJacobsthalSequence();
+  elementSize = 1;
+  jacobsthalIndex = 3;
+  keepRecursing = true;
+}
+
+template <typename Container> PmergeMe<Container>::~PmergeMe() {}
+
+template <typename Container>
+void PmergeMe<Container>::createJacobsthalSequence() {
+  size_t sequenceSize = 20;
+  jacobsthalSequence.push_back(0);
+  jacobsthalSequence.push_back(1);
+  for (size_t i = 2; i < sequenceSize; ++i) {
+    jacobsthalSequence.push_back(jacobsthalSequence[i - 1] +
+                                 2 * jacobsthalSequence[i - 2]);
+  }
+}
+
+// Take the smallest pair + the larger elements from every other pair
+template <typename Container> void PmergeMe<Container>::formMainSequence() {
+  seq.clear();
+  seq.insert(seq.end(), pairs[0].small.begin(), pairs[0].small.end());
+  size_t index = 2 * elementSize - 1;
+  pairs[0].indexInMainSequence = index;
+  seq.insert(seq.end(), pairs[0].large.begin(), pairs[0].large.end());
+  pairs.erase(pairs.begin());
+  for (size_t i = 0; i < pairs.size(); ++i) {
+    seq.insert(seq.end(), pairs[i].large.begin(), pairs[i].large.end());
+    index += elementSize;
+    pairs[i].indexInMainSequence = index;
+  }
+}
+
+// Returns the index in pairs from where we start inserting
+template <typename Container>
+void PmergeMe<Container>::updatePairIndexes(int inserted) {
+  for (size_t j = 0; j < pairs.size(); ++j) {
+    if (pairs[j].large.back() > inserted) {
+      pairs[j].indexInMainSequence += elementSize;
+    }
+  }
+}
+
+template <typename Container> int PmergeMe<Container>::getNextJacobsthal() {
+  int jacobSthal = jacobsthalSequence[jacobsthalIndex] -
+                   jacobsthalSequence[jacobsthalIndex - 1];
+  jacobsthalIndex++;
+  if (jacobSthal >= static_cast<int>(pairs.size())) {
+    jacobSthal = pairs.size();
+  }
+  return jacobSthal - 1;
+}
+
+template <typename Container> void PmergeMe<Container>::binaryInsertionSort() {
+  if (elementSize * 2 > seq.size()) {
+    return;
+  }
+
+  formMainSequence();
+
+  while (!pairs.empty()) {
+    int i = getNextJacobsthal();
+    while (i >= 0) {
+      int rightBoundary = pairs[i].indexInMainSequence / elementSize;
+      // Perform binary search for insertion point
+      int left = 0, right = rightBoundary;
+      while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (pairs[i].small.back() > seq[mid * elementSize + elementSize - 1]) {
+          left = mid + 1;
+        } else {
+          right = mid;
+        }
+      }
+
+      // Calculate the actual index for insertion, and insert the sequence
+      int insertIndex = left * elementSize;
+      seq.insert(seq.begin() + insertIndex, pairs[i].small.begin(),
+                 pairs[i].small.end());
+
+      updatePairIndexes(pairs[i].small.back());
+      pairs.erase(pairs.begin() + i);
+      --i;
     }
   }
 
-private:
-  std::vector<int> vec;
-  std::vector<Pair> pairVec;
-  std::vector<int> oddElementVec;
-  std::vector<int> leftoversVec;
+  // Insert the odd element if one exists
+  if (!oddElement.empty()) {
+    int rightBoundary = (seq.size() - 1) / elementSize;
+    int left = 0, right = rightBoundary;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (oddElement.back() > seq[mid * elementSize + elementSize - 1]) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    int insertIndex = left * elementSize;
+    seq.insert(seq.begin() + insertIndex, oddElement.begin(), oddElement.end());
+  }
 
-  std::deque<int> deq;
-  std::deque<Pair> pairDeq;
-  std::deque<int> oddElementDeq;
-  std::deque<int> leftoversDeq;
+  // Add the leftover numbers to the end of the seqtor
+  for (size_t i = 0; i < leftovers.size(); ++i) {
+    seq.push_back(leftovers[i]);
+  }
+}
 
-  std::vector<size_t> jacobsthalSequence;
-  size_t jacobsthalIndex;
-  size_t elementSize;
-  bool keepRecursing;
-};
+template <typename Container> void PmergeMe<Container>::makePairs() {
+  pairs.clear();
+  oddElement.clear();
+  leftovers.clear();
+  size_t i;
+  for (i = 0; i + elementSize * 2 <= seq.size(); i += elementSize * 2) {
+    Pair pair;
+    pair.small.insert(pair.small.end(), seq.begin() + i,
+                      seq.begin() + i + elementSize);
+    pair.large.insert(pair.large.end(), seq.begin() + i + elementSize,
+                      seq.begin() + i + elementSize * 2);
+    pairs.push_back(pair);
+  }
+  if (i <= seq.size() - elementSize) {
+    oddElement.insert(oddElement.end(), seq.begin() + i,
+                      seq.begin() + i + elementSize);
+    i += elementSize;
+  }
+  if (i < seq.size()) {
+    leftovers.insert(leftovers.end(), seq.begin() + i, seq.end());
+  }
+}
+
+template <typename Container> void PmergeMe<Container>::sortPairs() {
+  for (size_t i = 0; i < pairs.size(); ++i) {
+    if (!pairs[i].small.empty() && !pairs[i].large.empty() &&
+        pairs[i].small.back() > pairs[i].large.back()) {
+      std::swap(pairs[i].small, pairs[i].large);
+      // Sort the pair in the seqtor
+      std::swap_ranges(seq.begin() + i * elementSize * 2,
+                       seq.begin() + i * elementSize * 2 + elementSize,
+                       seq.begin() + i * elementSize * 2 + elementSize);
+    }
+  }
+}
+
+template <typename Container> void PmergeMe<Container>::fordJohnsonAlgorithm() {
+  if (keepRecursing && elementSize * 2 <= seq.size()) {
+    makePairs();
+    sortPairs();
+    elementSize *= 2;
+    fordJohnsonAlgorithm(); // Recurse
+  }
+
+  keepRecursing = false;
+  makePairs();
+  binaryInsertionSort();
+  elementSize /= 2;
+  jacobsthalIndex = 3;
+  if (jacobsthalIndex >= jacobsthalSequence.size()) {
+    throw std::out_of_range("Jacobsthal index out of range");
+  }
+}
+
+template <typename Container>
+Container PmergeMe<Container>::sort(const Container &input) {
+  seq = input;
+  fordJohnsonAlgorithm();
+  return seq;
+}
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lkonttin <lkonttin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 12:21:59 by lkonttin          #+#    #+#             */
-/*   Updated: 2025/01/21 17:33:24 by lkonttin         ###   ########.fr       */
+/*   Updated: 2025/01/29 21:51:41 by lkonttin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,25 @@
 #include <iostream>
 #include <stdexcept>
 
+template <typename Container> void printSequence(const Container &cont) {
+  for (typename Container::const_iterator it = cont.begin(); it != cont.end();
+       ++it) {
+    if (it != cont.begin())
+      std::cout << " ";
+    std::cout << *it;
+  }
+  std::cout << std::endl;
+}
+
 static int validateInput(int argc, char **argv) {
-  for (int i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; ++i) {
     try {
       std::stoi(argv[i]);
     } catch (std::invalid_argument &e) {
-      std::cerr << "Invalid input: " << e.what() << std::endl;
+      std::cerr << "Error: Invalid input '" << argv[i] << "'" << std::endl;
+      return 1;
+    } catch (std::out_of_range &e) {
+      std::cerr << "Error: Out of range input '" << argv[i] << "'" << std::endl;
       return 1;
     }
   }
@@ -28,49 +41,47 @@ static int validateInput(int argc, char **argv) {
 }
 
 static int sortWithVector(int argc, char **argv) {
-  PmergeMe pm;
+  PmergeMe<std::vector<int>> pm;
   std::vector<int> vec;
   std::vector<int> sorted;
+
   try {
     auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; ++i)
       vec.push_back(std::stoi(argv[i]));
-    }
-    sorted = pm.sortVector(vec);
+    sorted = pm.sort(vec);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
+
     std::cout << "Before: ";
-    pm.printSequence(vec);
+    printSequence(vec);
     std::cout << "After: ";
-    pm.printSequence(sorted);
+    printSequence(sorted);
     std::cout << "Time to process a range of " << vec.size()
-              << " elements with std::vector: " << elapsed.count() << "s"
-              << std::endl;
+              << " elements with std::vector: " << elapsed.count() << "s\n";
   } catch (std::exception &e) {
-    std::cerr << "Error: " << e.what() << std::endl;
+    std::cerr << "Error (vector): " << e.what() << std::endl;
     return 1;
   }
   return 0;
 }
 
 static int sortWithDeque(int argc, char **argv) {
-  return 0;
-  PmergeMe pm;
+  PmergeMe<std::deque<int>> pm;
   std::deque<int> deq;
   std::deque<int> sorted;
+
   try {
     auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; ++i)
       deq.push_back(std::stoi(argv[i]));
-    }
-    sorted = pm.sortDeque(deq);
+    sorted = pm.sort(deq);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
     std::cout << "Time to process a range of " << deq.size()
-              << " elements with std::deque: " << elapsed.count() << "s"
-              << std::endl;
+              << " elements with std::deque: " << elapsed.count() << "s\n";
   } catch (std::exception &e) {
-    std::cerr << "Error: " << e.what() << std::endl;
+    std::cerr << "Error (deque): " << e.what() << std::endl;
     return 1;
   }
   return 0;
@@ -78,14 +89,12 @@ static int sortWithDeque(int argc, char **argv) {
 
 int main(int argc, char **argv) {
   if (argc < 2) {
-    std::cerr << "Usage: ./PMergeMe [integers...]" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " [integers...]\n";
     return 1;
   }
-  if (validateInput(argc, argv)) {
+  if (validateInput(argc, argv) != 0)
     return 1;
-  }
-  if (sortWithVector(argc, argv) || sortWithDeque(argc, argv)) {
+  if (sortWithVector(argc, argv) || sortWithDeque(argc, argv))
     return 1;
-  }
   return 0;
 }
